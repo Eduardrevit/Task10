fa88d6f73aed0e784e13caaf0b7abed0529fb390
Эдуард <@LAPTOP-2C6APU5G>
2022-12-07 00:12:23 +0300

    Добавьте файлы проекта.

---
+++ Task10/Corner.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10
+{
+    class Corner
+    {
+        int gradus;
+        int min;
+        int sec;
+        public bool isCorrect = true;
+
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                if (value < 0)
+                isCorrect = false;
+                else
+                    gradus = value % 360;
+            }
+        }
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                    Gradus += value / 60;    
+                    min = value % 60;
+            }
+        }
+        public int Sec
+        {
+            get  
+            {
+                return sec;
+            }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                
+                else
+                {  
+                        Min += value / 60;
+                    sec = value % 60;
+                }
+            }
+        }
+        public Corner(int gradus, int min, int sec)
+        {
+            this.gradus = gradus;
+            this.min = min;
+            this.sec = sec;
+        }
+        public double ToRadians()
+        {
+           return gradus * Math.PI / 180 + min * Math.PI / (180 * 60) + sec * Math.PI / (180 * 60 * 60);
+        }
+    }
+}
---
+++ Task10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Corner corner;
+            do
+            {
+                Console.WriteLine("Введите данные");
+                int g = Convert.ToInt32(Console.ReadLine());
+                int m = Convert.ToInt32(Console.ReadLine());
+                int s = Convert.ToInt32(Console.ReadLine());
+
+                corner = new Corner(g, m, s);
+            }
+            while (corner.isCorrect == false);
+
+            double resultRadians = corner.ToRadians();
+            Console.WriteLine(resultRadians);
+            Console.ReadKey();
+
+
+        }
+    }
+}
